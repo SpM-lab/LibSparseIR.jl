915438d573a4f55414ae4aca7835ed3616e3551c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-06-07 10:14:40 +0900

    Move functions to lib that uses C_API

--- src/lib/sampling.jl
+++ src/lib/sampling.jl
@@ -129,3 +129,200 @@ function MatsubaraSampling(basis::AbstractBasis; positive_only=false, sampling_p
     return MatsubaraSampling{eltype(sampling_points),typeof(basis)}(ptr, sampling_points, positive_only, basis)
 end
 
+# Common interface functions
+
+# sampling_points and basis are already defined in abstract.jl
+
+"""
+    npoints(sampling::AbstractSampling)
+
+Get the number of sampling points.
+"""
+function npoints(sampling::Union{TauSampling,MatsubaraSampling})
+    n_points = Ref{Int32}(-1)
+    ret = C_API.spir_sampling_get_npoints(sampling.ptr, n_points)
+    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get number of sampling points")
+    return Int(n_points[])
+end
+
+# Evaluation and fitting functions
+
+"""
+    evaluate(sampling::AbstractSampling, al::AbstractArray; dim=1)
+
+Evaluate basis coefficients at the sampling points using the C API.
+
+For multidimensional arrays, `dim` specifies which dimension corresponds to the basis coefficients.
+"""
+function evaluate(sampling::Union{TauSampling,MatsubaraSampling}, al::AbstractArray{T,N}; dim=1) where {T,N}
+    # Determine output dimensions
+    output_dims = collect(size(al))
+    output_dims[dim] = npoints(sampling)
+
+    # Determine output type based on sampling type
+    if sampling isa TauSampling
+        # For complex input, TauSampling should produce complex output
+        output_type = T
+        output = Array{output_type,N}(undef, output_dims...)
+        evaluate!(output, sampling, al; dim=dim)
+    else # MatsubaraSampling
+        output_type = T <: Real ? ComplexF64 : promote_type(ComplexF64, T)
+        output = Array{output_type,N}(undef, output_dims...)
+        evaluate!(output, sampling, al; dim=dim)
+    end
+
+    return output
+end
+
+"""
+    evaluate!(output::AbstractArray, sampling::AbstractSampling, al::AbstractArray; dim=1)
+
+In-place version of [`evaluate`](@ref). Write results to the pre-allocated `output` array.
+"""
+function evaluate!(output::AbstractArray{Tout,N}, sampling::TauSampling, al::AbstractArray{Tin,N}; dim=1) where {Tout,Tin,N}
+    # Check dimensions
+    expected_dims = collect(size(al))
+    expected_dims[dim] = npoints(sampling)
+    size(output) == tuple(expected_dims...) || throw(DimensionMismatch("Output array has wrong dimensions"))
+
+    # Prepare arguments for C API
+    ndim = N
+    input_dims = Int32[size(al)...]
+    target_dim = Int32(dim - 1)  # C uses 0-based indexing
+    order = C_API.SPIR_ORDER_COLUMN_MAJOR
+
+    # Call appropriate C function based on input/output types
+    if Tin <: Real && Tout <: Real
+        ret = C_API.spir_sampling_eval_dd(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    elseif Tin <: Complex && Tout <: Complex
+        ret = C_API.spir_sampling_eval_zz(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    else
+        error("Type combination not yet supported for TauSampling: input=$Tin, output=$Tout")
+    end
+
+    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate sampling: status=$ret")
+    return output
+end
+
+function evaluate!(output::AbstractArray{Tout,N}, sampling::MatsubaraSampling, al::AbstractArray{Tin,N}; dim=1) where {Tout,Tin,N}
+    # Check dimensions
+    expected_dims = collect(size(al))
+    expected_dims[dim] = npoints(sampling)
+    size(output) == tuple(expected_dims...) || throw(DimensionMismatch("Output array has wrong dimensions"))
+
+    # Prepare arguments for C API
+    ndim = N
+    input_dims = Int32[size(al)...]
+    target_dim = Int32(dim - 1)  # C uses 0-based indexing
+    order = C_API.SPIR_ORDER_COLUMN_MAJOR
+
+    # Call appropriate C function based on input/output types
+    if Tin <: Real && Tout <: Complex
+        ret = C_API.spir_sampling_eval_dz(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    elseif Tin <: Complex && Tout <: Complex
+        ret = C_API.spir_sampling_eval_zz(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    else
+        error("Type combination not supported for MatsubaraSampling: input=$Tin, output=$Tout")
+    end
+
+    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate sampling: status=$ret")
+    return output
+end
+
+"""
+    fit(sampling::AbstractSampling, al::AbstractArray; dim=1)
+
+Fit basis coefficients from values at sampling points using the C API.
+
+For multidimensional arrays, `dim` specifies which dimension corresponds to the sampling points.
+"""
+function fit(sampling::Union{TauSampling,MatsubaraSampling}, al::AbstractArray{T,N}; dim=1) where {T,N}
+    # Determine output dimensions
+    output_dims = collect(size(al))
+    output_dims[dim] = length(sampling.basis)
+
+    # Determine output type - typically real for coefficients
+    if sampling isa TauSampling
+        # For complex input, we need complex output
+        output_type = T
+    else # MatsubaraSampling
+        # For Matsubara sampling, we need to be careful about type matching
+        # The C API might expect complex output even for real input
+        output_type = T <: Complex ? T : ComplexF64
+    end
+
+    output = Array{output_type,N}(undef, output_dims...)
+    fit!(output, sampling, al; dim=dim)
+
+    # For MatsubaraSampling, if we want real coefficients, extract real part
+    if sampling isa MatsubaraSampling && T <: Complex && output_type <: Complex
+        # The fitted coefficients should be real for physical reasons
+        # Extract real part and return as real array
+        real_output = Array{real(output_type),N}(undef, output_dims...)
+        real_output .= real.(output)
+        return real_output
+    end
+
+    return output
+end
+
+"""
+    fit!(output::AbstractArray, sampling::AbstractSampling, al::AbstractArray; dim=1)
+
+In-place version of [`fit`](@ref). Write results to the pre-allocated `output` array.
+"""
+function fit!(output::AbstractArray{Tout,N}, sampling::TauSampling, al::AbstractArray{Tin,N}; dim=1) where {Tout,Tin,N}
+    # Check dimensions
+    expected_dims = collect(size(al))
+    expected_dims[dim] = length(sampling.basis)
+    size(output) == tuple(expected_dims...) || throw(DimensionMismatch("Output array has wrong dimensions"))
+
+    # Prepare arguments for C API
+    ndim = N
+    input_dims = Int32[size(al)...]
+    target_dim = Int32(dim - 1)  # C uses 0-based indexing
+    order = C_API.SPIR_ORDER_COLUMN_MAJOR
+
+    # Call appropriate C function
+    if Tin <: Real && Tout <: Real
+        ret = C_API.spir_sampling_fit_dd(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    elseif Tin <: Complex && Tout <: Complex
+        ret = C_API.spir_sampling_fit_zz(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    else
+        error("Type combination not yet supported for TauSampling fit: input=$Tin, output=$Tout")
+    end
+
+    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to fit sampling: status=$ret")
+    return output
+end
+
+function fit!(output::AbstractArray{Tout,N}, sampling::MatsubaraSampling, al::AbstractArray{Tin,N}; dim=1) where {Tout,Tin,N}
+    # Check dimensions
+    expected_dims = collect(size(al))
+    expected_dims[dim] = length(sampling.basis)
+    size(output) == tuple(expected_dims...) || throw(DimensionMismatch("Output array has wrong dimensions"))
+
+    # Prepare arguments for C API
+    ndim = N
+    input_dims = Int32[size(al)...]
+    target_dim = Int32(dim - 1)  # C uses 0-based indexing
+    order = C_API.SPIR_ORDER_COLUMN_MAJOR
+
+    # Call appropriate C function based on input/output types
+    if Tin <: Complex && Tout <: Complex
+        # Use complex-to-complex API and then extract real part if needed
+        ret = C_API.spir_sampling_fit_zz(sampling.ptr, order, ndim, input_dims, target_dim, al, output)
+    elseif Tin <: Complex && Tout <: Real
+        # Create temporary complex output, then extract real part
+        temp_output = Array{ComplexF64,N}(undef, size(output)...)
+        ret = C_API.spir_sampling_fit_zz(sampling.ptr, order, ndim, input_dims, target_dim, al, temp_output)
+        ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to fit sampling: status=$ret")
+        output .= real.(temp_output)
+        return output
+    else
+        error("Type combination not supported for MatsubaraSampling fit: input=$Tin, output=$Tout")
+    end
+
+    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to fit sampling: status=$ret")
+    return output
+end
--- src/spir/basis.jl
+++ src/spir/basis.jl
@@ -1,10 +1,3 @@
-# Override the length function from abstract.jl to work with our C API
-function Base.length(basis::FiniteTempBasis)
-    size_ref = Ref{Int32}(-1)
-    ret = C_API.spir_basis_get_size(basis.ptr, size_ref)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get basis size")
-    return Int(size_ref[])
-end
 
 # Property accessors
 β(basis::FiniteTempBasis) = basis.beta
@@ -15,139 +8,8 @@ end
 # In reality, it would be computed from the singular values
 accuracy(basis::FiniteTempBasis) = basis.epsilon
 
-# Default sampling points
-function default_tau_sampling_points(basis::FiniteTempBasis)
-    n_points = Ref{Int32}(-1)
-    ret = C_API.spir_basis_get_n_default_taus(basis.ptr, n_points)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get number of default tau points")
-
-    points = Vector{Float64}(undef, n_points[])
-    ret = C_API.spir_basis_get_default_taus(basis.ptr, points)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get default tau points")
-
-    return points
-end
-
-function default_matsubara_sampling_points(basis::FiniteTempBasis; positive_only=false)
-    n_points = Ref{Int32}(-1)
-    ret = C_API.spir_basis_get_n_default_matsus(basis.ptr, positive_only, n_points)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get number of default Matsubara points")
-
-    points = Vector{Int64}(undef, n_points[])
-    ret = C_API.spir_basis_get_default_matsus(basis.ptr, positive_only, points)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get default Matsubara points")
-
-    # Convert to MatsubaraFreq objects based on statistics
-    if statistics(basis) isa Fermionic
-        return [FermionicFreq(n) for n in points]
-    else
-        return [BosonicFreq(n) for n in points]
-    end
-end
-
-function default_omega_sampling_points(basis::FiniteTempBasis)
-    n_points = Ref{Int32}(-1)
-    ret = C_API.spir_basis_get_n_default_ws(basis.ptr, n_points)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get number of default omega points")
-
-    points = Vector{Float64}(undef, n_points[])
-    ret = C_API.spir_basis_get_default_ws(basis.ptr, points)
-    ret == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get default omega points")
-
-    return points
-end
-
-# Evaluate basis function at single point
-function (f::BasisFunction)(x::Real)
-    out = Vector{Float64}(undef, length(f.basis))
-    status = C_API.spir_funcs_eval(f.ptr, Float64(x), out)
-    status == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate basis functions")
-    return out
-end
-
-# Evaluate basis function at Matsubara frequency (for uhat)
-function (f::BasisFunction)(n::Integer)
-    out = Vector{ComplexF64}(undef, length(f.basis))
-    status = C_API.spir_funcs_eval_matsu(f.ptr, Int64(n), out)
-    status == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate basis functions at Matsubara frequency")
-    return out
-end
-
 function (f::BasisFunction)(freq::MatsubaraFreq)
     return f(freq.n)
-end
-
-# Evaluate at multiple points
-function (f::BasisFunction)(xs::AbstractVector{<:Real})
-    n_points = length(xs)
-    n_funcs = length(f.basis)
-    out = Matrix{Float64}(undef, n_points, n_funcs)
-
-    # Convert to column-major format expected by C API
-    status = C_API.spir_funcs_batch_eval(f.ptr, C_API.SPIR_ORDER_COLUMN_MAJOR,
-                                         n_points, collect(Float64, xs), vec(out))
-    status == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate basis functions at multiple points")
-    return out
-end
-
-# Evaluate at multiple Matsubara frequencies
-function (f::BasisFunction)(ns::AbstractVector{<:Integer})
-    n_points = length(ns)
-    n_funcs = length(f.basis)
-    out = Matrix{ComplexF64}(undef, n_points, n_funcs)
-
-    # Convert to column-major format expected by C API
-    status = C_API.spir_funcs_batch_eval_matsu(f.ptr, C_API.SPIR_ORDER_COLUMN_MAJOR,
-                                               n_points, collect(Int64, ns), vec(out))
-    status == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate basis functions at multiple Matsubara frequencies")
-    return out
-end
-
-function (f::BasisFunction)(freqs::AbstractVector{<:MatsubaraFreq})
-    ns = [freq.n for freq in freqs]
-    return f(ns)
-end
-
-# Get basis functions u, v, s, uhat
-function u(basis::FiniteTempBasis)
-    status = Ref{Int32}(-100)
-    ptr = C_API.spir_basis_get_u(basis.ptr, status)
-    status[] == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get u basis functions")
-    funcs = BasisFunction(ptr, basis)
-    finalizer(f -> C_API.spir_funcs_release(f.ptr), funcs)
-    return funcs
-end
-
-function v(basis::FiniteTempBasis)
-    status = Ref{Int32}(-100)
-    ptr = C_API.spir_basis_get_v(basis.ptr, status)
-    status[] == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get v basis functions")
-    funcs = BasisFunction(ptr, basis)
-    finalizer(f -> C_API.spir_funcs_release(f.ptr), funcs)
-    return funcs
-end
-
-function s(basis::FiniteTempBasis)
-    n = length(basis)
-    svals = Vector{Float64}(undef, n)
-    status = C_API.spir_basis_get_singular_values(basis.ptr, svals)
-    status == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get singular values")
-    return svals
-end
-
-function uhat(basis::FiniteTempBasis)
-    status = Ref{Int32}(-100)
-    ptr = C_API.spir_basis_get_uhat(basis.ptr, status)
-    status[] == C_API.SPIR_COMPUTATION_SUCCESS || error("Failed to get uhat basis functions")
-    funcs = BasisFunction(ptr, basis)
-    finalizer(f -> C_API.spir_funcs_release(f.ptr), funcs)
-    return funcs
-end
-
-# Additional utility functions
-function significance(basis::FiniteTempBasis)
-    svals = s(basis)
-    return svals / svals[1]
 end
 
 function rescale(basis::FiniteTempBasis{S}, new_beta::Real) where {S}
@@ -155,6 +17,12 @@ function rescale(basis::FiniteTempBasis{S}, new_beta::Real) where {S}
     new_lambda = Λ(basis) * new_beta / β(basis)
     kernel = LogisticKernel(new_lambda)
     return FiniteTempBasis{S}(kernel, new_beta, ωmax(basis), accuracy(basis))
+end
+
+# Additional utility functions
+function significance(basis::FiniteTempBasis)
+    svals = s(basis)
+    return svals / svals[1]
 end
 
 function finite_temp_bases(β::Real, ωmax::Real, ε=nothing; kernel=nothing, sve_result=nothing)
