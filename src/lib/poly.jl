d35f4afb61e19157a6c9dcb4d8e4ade8cd0fe932
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-06-11 11:58:16 +0900

    Move methods related to Ptr{spir_funcs} to poly.jl

--- src/lib/poly.jl
+++ src/lib/poly.jl
@@ -1,3 +1,35 @@
+function (funcs::Ptr{spir_funcs})(x::Real)
+    sz = Ref{Int32}(-1)
+    spir_funcs_get_size(funcs, sz) == SPIR_COMPUTATION_SUCCESS || error("Failed to get funcs size")
+    ret = Vector{Float64}(undef, Int(sz[]))
+    spir_funcs_eval(funcs, x, ret) == SPIR_COMPUTATION_SUCCESS || error("Failed to evaluate funcs")
+    return ret
+end
+
+function (funcs::Ptr{spir_funcs})(x::Vector{Float64})
+    hcat(funcs.(x)...)
+end
+
+function Base.getindex(funcs::Ptr{spir_funcs}, i::Int)
+    status = Ref{Int32}(-100)
+    indices = Vector{Int32}(undef, 1)
+    indices[1] = i
+    ret = spir_funcs_get_slice(funcs, 1, indices, status)
+    status[] == SPIR_COMPUTATION_SUCCESS || error("Failed to get basis function u $status[]")
+    return ret
+end
+
+Base.getindex(funcs::Ptr{spir_funcs}, I) = [funcs[i] for i in I]
+
+function Base.length(funcs::Ptr{spir_funcs})
+    sz = Ref{Int32}(-1)
+    spir_funcs_get_size(funcs, sz) == SPIR_COMPUTATION_SUCCESS || error("Failed to get funcs size")
+    return Int(sz[])
+end
+
+Base.firstindex(funcs::Ptr{spir_funcs}) = 1
+Base.lastindex(funcs::Ptr{spir_funcs}) = length(funcs)
+
 function roots(poly)
 	nroots_ref = Ref{Int32}(-1)
 	LibSparseIR.C_API.spir_funcs_get_n_roots(poly, nroots_ref)
